cc900f67dd4dd4ba148b9c3496fb87b407ab6214
rgp88 <ragiphasani@gmail.com>
2023-01-01 22:59:11 +0100

    allmost completed, need fix roundplayed

--- game.js
+++ game.js
@@ -6,6 +6,23 @@ let computerWins = 0;
 let roundPlayed = 1;
 
 
+
+const resultInfo = document.querySelector('.resultInfo');
+const resultScoreUser2 = document.querySelector('.resultScoreUser2');
+const resultScoreComp2 = document.querySelector('.resultScoreComp2');
+const roundBox =document.querySelector('.roundBox');
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector(".scissor");
+const input = document.querySelector(".inputs");
+const description = document.querySelector(".description");
+const resetButton = document.createElement("div");
+const main = document.querySelector(".main");
+const compPick = document.getElementById('compPick');
+
+roundBox.textContent = "Round Nr: "+roundPlayed;
+
 function getComputerChoice() {    
     let computerChoiceTemp  = ['rock','paper','scissor'];
     let num = (Math.random() * ((3 - 0) + 0));
@@ -14,29 +31,20 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-const resultInfo = document.querySelector(".resultInfo");
-
 function gameLogic() {
     if (userChoice == 'rock' && computerChoice == 'rock' || userChoice == 'paper' && computerChoice == 'paper' || userChoice == 'scissor' && computerChoice == 'scissor') {
-        console.log("Round:"+(roundPlayed));
-        console.log('User Choose: '+userChoice+' | Computer Choose: '+computerChoice)
-        console.log('This round is a draw');
-        console.log('User Wins: '+userWins+' | Computer Wins: '+computerWins);
-        console.log('------------------------------------------------');
+        userWins += 0.5;
+        computerWins += 0.5;
+        resultScoreUser2.textContent = userWins;
+        resultScoreComp2.textContent = computerWins;
     } else if (userChoice == 'rock' && computerChoice == 'scissor' || userChoice == 'paper' && computerChoice == 'rock' || userChoice == 'scissor' && computerChoice == 'rock') {
-        console.log("Round:"+(roundPlayed));
-        console.log('User Choose: '+userChoice+' | Computer Choose: '+computerChoice)
-        console.log('Player Wins this round');
         userWins++;
-        console.log('User Wins: '+userWins+' | Computer Wins: '+computerWins);
-        console.log('------------------------------------------------');
+        resultScoreUser2.textContent = userWins;
+        resultScoreComp2.textContent = computerWins;
     } else if (userChoice == 'rock' && computerChoice == 'paper' || userChoice == 'paper' && computerChoice == 'scissor' || userChoice == 'scissor' && computerChoice == 'paper') {
-        console.log("Round:"+(roundPlayed));
-        console.log('User Choose: '+userChoice+' | Computer Choose: '+computerChoice)
-        console.log('Computer Wins this round');
         computerWins++;
-        console.log('User Wins: '+userWins+' | Computer Wins: '+computerWins);
-        console.log('------------------------------------------------');
+        resultScoreUser2.textContent = userWins;
+        resultScoreComp2.textContent = computerWins;
     } 
 }
 
@@ -46,11 +54,21 @@ function game() {
         gameLogic();
     } else if (roundPlayed==5) {
         gameLogic();
-    gameOver(); }
+        roundPlayed++;
+        setTimeout(() => {
+        gameOver();
+        input.parentElement.removeChild(input);
+        document.querySelector(".comp").removeChild(document.querySelector(".compup"));
+        description.textContent = "Wanna play another game?";
+        resetButton.textContent = "Click Here!";
+        resetButton.classList.add("reset")
+        main.insertBefore(resetButton,description.nextSibling)
+        }, 900);
+    }
 }
 
+
 function gameOver() {
-    console.log('game over')
     if (userWins>computerWins) {
         resultInfo.textContent = ('You are the winner');
      } else if (userWins===computerWins) {
@@ -58,39 +76,38 @@ function gameOver() {
      } else {
         resultInfo.textContent = ("Computer WINS");
      }
-     roundPlayed = 6;
 }
 
 
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissor = document.querySelector(".scissor");
-const input = document.querySelector(".inputs");
    
 
 rock.addEventListener('click', ()=> {
+    if(roundPlayed<=5) {
     userChoice = 'rock';
     input.removeChild(paper);
     input.removeChild(scissor);
     getComputerChoice();
     if (computerChoice === "rock") {
-        document.getElementById('compPick').src = 'pic/rock.png';
+        compPick.src = 'pic/rock.png';
     } else if (computerChoice === "paper") {
-        document.getElementById('compPick').src = 'pic/paper.png';
+        compPick.src = 'pic/paper.png';
     } else {
-        document.getElementById('compPick').src = 'pic/scissor.png';
+        compPick.src = 'pic/scissor.png';
     }
     setTimeout(() => {
         input.appendChild(scissor);   
         input.insertBefore(paper,scissor);    
         document.getElementById('compPick').src = 'pic/thinking.png';
+        roundBox.textContent = "Round Nr: "+(roundPlayed);
         }, 1000);
         game();
         roundPlayed++;
+    }
     });
 
 
 paper.addEventListener('click', ()=> {
+    if(roundPlayed<=5) {
     userChoice = 'paper';
     input.removeChild(rock);
     input.removeChild(scissor);
@@ -106,12 +123,15 @@ paper.addEventListener('click', ()=> {
     input.insertBefore(rock,paper);
     input.appendChild(scissor);       
     document.getElementById('compPick').src = 'pic/thinking.png';
+    roundBox.textContent = "Round Nr: "+(roundPlayed);
     }, 1000);
     game();
     roundPlayed++;
+    }
     });
 
 scissor.addEventListener('click', ()=> {
+    if(roundPlayed<=5) {
     userChoice = 'scissor';
     input.removeChild(paper);
     input.removeChild(rock);
@@ -127,19 +147,36 @@ scissor.addEventListener('click', ()=> {
     input.insertBefore(paper,scissor);
     input.insertBefore(rock,paper);       
     document.getElementById('compPick').src = 'pic/thinking.png';
+    roundBox.textContent = "Round Nr: "+(roundPlayed);
     }, 1000);
     game();
     roundPlayed++;
+    }
     });
 
 
 const inputat = document.querySelectorAll(".inputat");
 
 inputat.forEach(itemi => {
-    itemi.addEventListener('mousedown', ()=> {
-        itemi.style.backgroundColor = "green";
-    itemi.addEventListener('mouseup', ()=> {
-        itemi.style.backgroundColor = "";
-    })
-    })
+    itemi.addEventListener('mousedown', mouseDown);
+    itemi.addEventListener('mouseup', mouseUp);
 });
+
+
+resetButton.addEventListener('mousedown', mouseDown);
+resetButton.addEventListener('mouseup', mouseUp);
+resetButton.addEventListener('click',newGame);
+
+function mouseDown() {
+        this.style.backgroundColor = "green";
+        this.style.borderColor = "grey";
+}   
+
+function mouseUp() {
+    this.style.backgroundColor = "";
+    this.style.borderColor = "";
+}
+
+function newGame() {
+    window.location.reload();
+}
